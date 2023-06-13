03081779d8be77ddcec3e09586866489a8079a9e
jakbin <jakbin4747@gmail.com>
2023-06-13 18:10:48 +0530

    update code for python-telegram-bot 20

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import requests
 import urllib.parse as urlparse
 from requests.exceptions import HTTPError
 
-from telegram import ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import ForceReply, Update
 from telegram.ext import (
-    Updater,
+    Application,
     CommandHandler,
     MessageHandler,
-    Filters,
+    filters,
     ConversationHandler,
     CallbackContext,
 )
@@ -20,15 +20,15 @@ from telegram.ext import (
 urllib3.disable_warnings()
 
 # Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+# logging.basicConfig(
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+# )
 
 logger = logging.getLogger(__name__)
 
 UPLOAD, DOWNLOAD = range(2)
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
@@ -36,7 +36,7 @@ def start(update: Update, context: CallbackContext) -> None:
         reply_markup=ForceReply(),
     )
 
-def upload(update: Update, context: CallbackContext):
+async def upload(update: Update, context: CallbackContext):
     """Starts the conversation and asks the user for url"""
     update.message.reply_text("Enter your url :- ")
 
@@ -79,7 +79,7 @@ def detect_filename(url=None, headers=None):
         names["headers"] = filename_from_headers(headers) or ''
     return names["out"] or names["headers"] or names["url"]
 
-def change_filename(update: Update, context: CallbackContext):
+async def change_filename(update: Update, context: CallbackContext):
     """Stores the selected url and asks for change file."""
     user = update.message.from_user
     url = update.message.text
@@ -134,7 +134,7 @@ def file_remover(filename: str):
          print("file not found.")
 
 
-def download(update: Update, context: CallbackContext):
+async def download(update: Update, context: CallbackContext):
     """download file and upload file with given name"""
     user = update.message.from_user
     url = context.user_data.get("url", 'Not found')
@@ -153,7 +153,7 @@ def download(update: Update, context: CallbackContext):
     return ConversationHandler.END
 
 
-def skip_download(update: Update, context: CallbackContext):
+async def skip_download(update: Update, context: CallbackContext):
     """Skip change file name, download and upload file with default name"""
     user = update.message.from_user
     url = context.user_data.get("url", 'Not found')
@@ -171,7 +171,7 @@ def skip_download(update: Update, context: CallbackContext):
 
     return ConversationHandler.END
 
-def cancel(update: Update, context: CallbackContext) -> int:
+async def cancel(update: Update, context: CallbackContext) -> int:
     """Cancels and ends the conversation."""
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
@@ -184,27 +184,25 @@ def cancel(update: Update, context: CallbackContext) -> int:
 
 def main() -> None:
     """Run the bot."""
-    updater = Updater("TOKEN")
+    application = Application.builder().token("TOKEN").build()
 
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # Add conversation handler with the states UPLOAD and DOWNLOAD
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('upload', upload)],
         states={
-            UPLOAD: [MessageHandler(Filters.entity('url'), change_filename)],
-            DOWNLOAD: [MessageHandler(Filters.text & ~Filters.command, download), CommandHandler('skip', skip_download)],
+            UPLOAD: [MessageHandler(filters.Entity('url') | filters.Document.ALL, change_filename)],
+            DOWNLOAD: [MessageHandler(filters.TEXT & ~filters.COMMAND, download), CommandHandler('skip', skip_download)],
         },
         fallbacks=[CommandHandler('cancel', cancel)],
     )
 
-    dispatcher.add_handler(conv_handler)
+    application.add_handler(conv_handler)
 
-    updater.start_polling()
+    # Run the bot until the user presses Ctrl-C
 
-    updater.idle()
+    application.run_polling()
 
 
 if __name__ == '__main__':
